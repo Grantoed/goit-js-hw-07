9ec3721a5e91c810fba077735c44631e7e0addd1
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-02 20:44:11 +0300

    task 2 solved

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<a class='gallery__item' href="${item.original}"><img class='gallery__image' src='${item.preview}' alt='${item.description}' width='408' height='272'/></a>`
+  )
+  .join("");
+
+galleryRef.innerHTML = markup;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250ms",
+});
+gallery.on("show.simplelightbox");
